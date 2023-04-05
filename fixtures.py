1ecf6c9ac5a3bdfb28daa6609b19a3d4c2b778ad
demonslayer68 <demonslayer68@gmail.com>
2023-04-05 15:53:23 +0530

    add proposed transfer as csv, minor clean-up

--- fixtures.py
+++ fixtures.py
@@ -13,27 +13,20 @@ from collections import Counter
 from collections import OrderedDict
 
 
-# In[ ]:
-
-
-pd.read_json( 'fixtures.json' )[ 'Data' ].Value
-
-
-# In[3]:
-
-
-fixtures = pd.json_normalize( pd.read_json( 'fixtures.json' )[ 'Data' ].Value )
-
-
 # In[4]:
 
 
+# read and format fixtures data
+# got fixtures json from the webpage
+pd.read_json( 'fixtures.json' )[ 'Data' ].Value
+fixtures = pd.json_normalize( pd.read_json( 'fixtures.json' )[ 'Data' ].Value )
 fixtures = fixtures[[ 'MatchId', 'HomeTeamShortName', 'AwayTeamShortName' ]]
 
 
 # In[5]:
 
 
+# default values for nextmatch
 fixtures['HomeNextMatch'] = 10
 fixtures['AwayNextMatch'] = 10
 
@@ -41,6 +34,7 @@ fixtures['AwayNextMatch'] = 10
 # In[6]:
 
 
+# populate next match
 for i in range(len(fixtures)):
     for team in [ fixtures['HomeTeamShortName'][i], fixtures['AwayTeamShortName'][i] ]:
         for j in range(i+1,len(fixtures)):
@@ -55,45 +49,40 @@ for i in range(len(fixtures)):
 # In[7]:
 
 
+# pd options to see full data
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
  
-# All dataframes hereafter reflect these changes.
-display(fixtures)
+#display(fixtures)
 
 
-# In[8]:
+# In[13]:
 
+
+# Get distribution of next match distance
 
 NextArray = list(fixtures['AwayNextMatch'].values) + list( fixtures['HomeNextMatch'].values )
-
-
-# In[9]:
-
-
-len(NextArray)
-
-
-# In[10]:
-
-
+print('total number of matches * 2:', len(NextArray))
 count = Counter(NextArray)
 count = dict(count)
-
-
-# In[11]:
-
-
 count = OrderedDict(sorted(count.items()))
 count
 
 
-# In[12]:
+# In[14]:
 
 
+# map of number of transfers for each next match distance
+# 1 and 10 are bogus so putting 0
 transfers = { 1 : 0, 2 : 4, 3 : 4, 4 : 1, 5 : 1, 6: 0, 7 : 0, 8 : 0, 10 : 0 }
+
+
+# In[15]:
+
+
+# populate populate transfers by match
 home_transfers = [ transfers[x] for x in fixtures['HomeNextMatch'].values ]
 away_transfers = [ transfers[x] for x in fixtures['AwayNextMatch'].values ]
 fixtures[ 'HomeTransfers' ] = home_transfers
@@ -101,40 +90,22 @@ fixtures[ 'AwayTransfers' ] = away_transfers
 fixtures[ 'TotalTransfers' ] = fixtures[ 'AwayTransfers' ] + fixtures[ 'HomeTransfers' ] 
 
 
-# In[13]:
+# In[16]:
 
 
-fixtures['TotalTransfers'].sum()
+print( 'number of proposed transfers: ', fixtures['TotalTransfers'].sum() )
 
 
-# In[14]:
+# In[19]:
 
 
-fixtures
-
-
-# In[31]:
-
-
-3 * 0 + 1 * 3 + 18 * 3 + 31 * 1 + 31 * 1 + 33 * 0 + 11 * 0 + 5 * 0 + 15 * 0
+fixtures.to_csv('proposed_transfers.csv')
 
 
 # In[20]:
 
 
-sum(list(count.values()))
-
-
-# In[ ]:
-
-
-
-
-
-# In[21]:
-
-
-31 * 1 + 11 * 0 + 31 * 1 + 33 * 0 + 5 * 0 + 18 * 3 + 1 * 5 + 10 * 0
+fixtures
 
 
 # In[ ]:
